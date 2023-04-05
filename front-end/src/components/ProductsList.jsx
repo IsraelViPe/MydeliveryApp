a0d61f1610be1292a407e191fc6e066bfdc5bbc1
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-04-05 12:38:29 -0300

    refactor: Nova Refatoração Carrinho de Compras

--- front-end/src/components/ProductsList.jsx
+++ front-end/src/components/ProductsList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getAllProducts, verifyToken } from '../Services/RequestAPI';
 import ProductCard from './ProductCard';
+import CheckoutButtonValue from './CheckoutButtonValue';
 
 export default function ProductsList() {
   const [products, setProducts] = useState([]);
@@ -31,6 +32,7 @@ export default function ProductsList() {
       { products.length > 0 && products.map((product, i) => (
         <ProductCard product={ product } key={ `prod-${i}` } />
       ))}
+      <CheckoutButtonValue />
     </main>
   );
 }
