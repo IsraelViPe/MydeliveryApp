d8e837dc2cd2d6eda12c0838e73bb880245378db
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-05 17:44:50 -0300

    feat : cria o endpoint de busca de todas as vendas

--- back-end/src/routes/sale.router.js
+++ back-end/src/routes/sale.router.js
@@ -4,5 +4,6 @@ const SalesController = require('../controllers/sales.controller');
 const router = express.Router();
 
 router.post('/', SalesController.create);
+router.get('/', SalesController.getAll);
 
 module.exports = router; 
