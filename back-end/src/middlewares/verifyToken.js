c89902631108a061981d4ffb2bc935753230648e
Robson Narcizo <rob.narcizo.27@gmail.com>
2023-04-05 17:27:04 -0300

    fix: Implementação verifyToken

--- back-end/src/middlewares/verifyToken.js
+++ back-end/src/middlewares/verifyToken.js
@@ -1,17 +1,23 @@
-import Jwt from 'jsonwebtoken';
-import fs from 'fs';
-import Path from 'path';
-import { mapError } from '../utils/errorMap';
+const Jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Path = require('path');
+const { mapError } = require('../utils/errorMap');
 
 const verifyToken = (req, res, next) => {
     const { authorization } = req.headers;
-    if (!authorization) next(mapError('Token not found'));
+    if (!authorization) { 
+        next(mapError('Token not found'));
+        return;
+    }
     const secret = fs.readFileSync(Path.resolve(__dirname, '..', '..', 'jwt.evaluation.key'));
     Jwt.verify(authorization, secret, (error, decoded) => {
-        if (error) next(mapError('Token inválido'));
+        if (error) {
+            next(mapError('Token inválido'));
+            return;
+        }
         req.user = decoded.sub;
         next();
     });
 };
 
-export default verifyToken;
+module.exports = verifyToken;
