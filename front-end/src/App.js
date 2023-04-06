8c8a9659734f40cf56468104a5b356ef8c60cf89
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-06 00:21:37 -0300

    feat: iniciando tela de detalhes do pedido

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/Login/Login';
 import Products from './pages/Products/Products';
 import Checkout from './pages/CustomerCheckout/Checkout';
 import CartProvider from './context/CartProvider';
+import OrderDetails from './pages/OrderDetails.js/OrderDetails';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
           <Route exact path="/login" component={ Login } />
           <Route exact path="/customer/products" component={ Products } />
           <Route exact path="/customer/checkout" component={ Checkout } />
+          <Route exact path="/customer/orders/:id" component={ OrderDetails } />
         </Switch>
       </CartProvider>
     </DeliveryProvider>
