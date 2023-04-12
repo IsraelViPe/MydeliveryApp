222037f491918dd8e10e3bc0f5cee6b64969c9a4
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-12 00:43:22 -0300

    feat: criando rotas para teste do funcionamento da tela adimin

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const UserService = require('../services/user.service');
+const User = require('../database/models');
 const { codes } = require('../utils/errorMap');
 
 const getAllSellers = async (_req, res, _next) => {
@@ -20,4 +21,34 @@ const getUserById = async (req, res, next) => {
   return res.status(codes.OK).json(response);
 };
 
-module.exports = { getAllSellers, getUserById };
+const getAllUsers = async (req, res, next) => {
+  try {
+    const response = await User.findAll();
+    res.status(codes.OK).json(response);
+  } catch (e) {
+    next(e);
+  }
+};
+
+  const AddUser = async (req, res, next) => {
+    try {
+      const response = await User.create(req.body);
+      res.status(codes.CREATED).json(response);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  const DeleteUser = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const response = await User.destroy({
+        where: { id },
+      });
+      res.status(codes.OK).json(response);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+module.exports = { getAllSellers, getUserById, getAllUsers, AddUser, DeleteUser };
--- back-end/src/routes/user.router.js
+++ back-end/src/routes/user.router.js
@@ -5,7 +5,10 @@ const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
-router.get('/sellers', UserController.getAllSellers);
+router.get('/sellers', UserController.getAllSellers); 
+router.get('/admin', verifyToken, UserController.getAllUsers);
+router.post('/admin', verifyToken, UserController.AddUser);
+router.delete('/admin/:id', verifyToken, UserController.DeleteUser);
 router.get('/:id', UserController.getUserById);
 router.get('/seller/orders', verifyToken, SalesController.getSalesBySeller);
 router.get('/customer/orders', verifyToken, SalesController.getSalesByCustomer);
