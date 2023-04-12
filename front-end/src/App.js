0d5f63c42be782d5d2de2e2fdd9ac5138b467280
IsraelPereira <pereira.israel2070@gmail.com>
2023-04-12 17:44:41 -0300

    fix: corringido importe faltando

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,6 +11,7 @@ import OrderDetails from './pages/OrderDetails.js/OrderDetails';
 import SellerOrderDetails from './pages/SellersOrderDetails/SellerOrderDetails';
 import SellerOrders from './pages/SellerOrders/SellerOrders';
 import './styles/global.css';
+import AdiminManager from './pages/AdminManager/AdminManager';
 
 export default function App() {
   return (
